380c81782210c38b58546cf581980b04686af582
Adheel Barlas <adheelb8@gmail.com>
2018-04-30 20:28:49 +0100

    Adjusted not found to contain link to home and onclick on table row to pass dynamic id for url to detail page which will show in depth stats of cryptocoin being clicked on

---
+++ src/components/detail/Detail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Detail.css';
+
+class Detail extends React.Component {
+    render(){
+        return(
+            <div>
+                Detail
+            </div>
+        );
+    }
+}
+
+export default Detail;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import './index.css'
 import List from './components/list/List';
+import NotFound from './components/notfound/notFound';
+import Detail from './components/detail/Detail';
 
 const App = () => {
     const title = 'React Coin';
@@ -15,6 +17,9 @@ const App = () => {
 
                 <Switch>
                     <Route path="/" component={List} exact />
+                    <Route path="/currency/:id" component={Detail} exact />
+
+                    <Route component={NotFound} />
                 </Switch>
             </div>
         </BrowserRouter>      
