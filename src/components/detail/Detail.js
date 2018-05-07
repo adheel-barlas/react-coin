3851467ac11ceb3ae51010292be596fd62c94aff
Adheel Barlas <adheelb8@gmail.com>
2018-05-07 19:07:42 +0100

    Adding dynamic request for detail

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { API_URL } from '../../config';
+import { handleResponse } from '../../helpers';
 import './Detail.css';
 
 class Detail extends React.Component {
+
+    componentDidMount(){
+        const currencyId = this.props.match.params.id;
+        
+        fetch(`${API_URL}/cryptocurrencies/${currencyId}`)
+            .then(this.handleResponse)
+            .then((currency) => {
+                console.log('currency', currency);
+            })
+            .catch((error) => {
+                console.log('error', error);
+            })
+    }
+
     render(){
         return(
             <div>
