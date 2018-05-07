90b1bcba3c797757e0d2c9e1a159e47f3d275395
Adheel Barlas <adheelb8@gmail.com>
2018-05-07 19:25:57 +0100

    Added loading state and accesses data from api and prints to page, full details still needed

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,27 +1,59 @@
 import React from 'react';
 import { API_URL } from '../../config';
+import Loading from '../common/Loading';
 import { handleResponse } from '../../helpers';
 import './Detail.css';
 
 class Detail extends React.Component {
 
+    constructor(){
+        super();
+
+        this.state = {
+            currency: {},
+            loading: false,
+            error: null,
+        };
+    }
+
     componentDidMount(){
         const currencyId = this.props.match.params.id;
         
         fetch(`${API_URL}/cryptocurrencies/${currencyId}`)
-            .then(this.handleResponse)
+            .then(handleResponse)
             .then((currency) => {
-                console.log('currency', currency);
+                this.setState({
+                    loading: false,
+                    error: null,
+                    currency,
+                });
             })
             .catch((error) => {
-                console.log('error', error);
+                this.setState({
+                    loading: false,
+                    error: error.errorMessage,
+                });
             })
     }
 
     render(){
+        const { loading, error, currency } = this.state;
+
+        console.log(currency);
+
+        // If state loading is set to true
+        if(loading){
+            return <div className="loading-container"><Loading /></div>
+        }
+
+        // If state error is set to true
+        if(error){
+            return <div className="error">{error}</div>
+        }
+
         return(
-            <div>
-                Detail
+            <div className="Detail">
+                <h1>{currency.name}</h1> {currency.symbol}
             </div>
         );
     }
